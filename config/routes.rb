5afd9cdcde341bcc5c05e8cb4a32fac10806edda
Olesya Bar <2olesya.baranova@gmail.com>
2023-04-30 21:52:38 -0400

    Lesson 3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#  get '/pages', to: 'pages#index'
+#  post '/pages', to: 'pages#create'
+#  get '/pages/new', to: 'pages#new', as: 'new_page'
+#  get '/pages/:id', to: 'pages#show', as: 'page'
+#  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+#  patch '/pages/:id', to: 'pages#update'
+#  delete '/pages/:id', to: 'pages#destroy'
+  resources :pages
 
   # Defines the root path route ("/")
   # root "articles#index"
